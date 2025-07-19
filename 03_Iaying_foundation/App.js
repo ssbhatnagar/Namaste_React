f2108c7bdb4f792632ecfe1f8ba870e81ac980e2
S S Bhatnagar <shubhambhatnagar072@gmail.com>
2025-07-20 03:35:48 +0530

    episode 3 laying foundation

--- 03_Iaying_foundation/App.js
+++ 03_Iaying_foundation/App.js
@@ -1,21 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client'; // Notice the /client for React 18+
 
-const parent = React.createElement("div", {id: "parent"},
-    [
-        React.createElement("div", {id: "child"}, 
-        [
-            React.createElement("h1", {}, "Im the H1 tag"), 
-            React.createElement("h2", {}, "Im the H2 tag")
-        ]
-    ), 
-        React.createElement("div", {id: "child2"}, 
-        [
-            React.createElement("h1", {}, "Im the H1 tag"), 
-            React.createElement("h2", {}, "Im the H2 tag")
-        ]
-    )
-    ]
-)
+// this is using react create elemement
+// const heading = React.createElement("h1", {id: "heading"}, "Namaste JS Episode 3");
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+// root.render(heading)
+
+// using jsx the above code can written like below
+// const jsxHeading = <h1 id='heading'> This heading is using JSX</h1>
+// root.render(jsxHeading)
+
+// React functional component 
+const TitleFunctionalComponent = () =>(
+    <h1 className='heading'> Namaste Js from component </h1>
+)
+
+const TitleElement = (
+    <h1 className='heading'> Namaste Js from element </h1>
+)
+
+const str = "This is how you can inject JS code in Compoenent using {}, This whole line is saved as string in JS code"
+
+// This is component composition => composing two components into one another the way TitleFunctionalComponent is used here
+const HeadingComponent = () =>(
+    <div>
+    <TitleFunctionalComponent/> 
+    {str} 
+    {TitleElement}
+    <h1 className='heading'>This is comming from functional component</h1>
+    </div>
+)
+
+root.render(<HeadingComponent/>) // this is how we render a component
