e9e5f1f334a5cb5d4be09de5ab14bc14a9c1f391
S S Bhatnagar <shubhambhatnagar072@gmail.com>
2025-07-20 00:44:36 +0530

    Episode 3

---
+++ 03_Iaying_foundation/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client'; // Notice the /client for React 18+
+
+const parent = React.createElement("div", {id: "parent"},
+    [
+        React.createElement("div", {id: "child"}, 
+        [
+            React.createElement("h1", {}, "Im the H1 tag"), 
+            React.createElement("h2", {}, "Im the H2 tag")
+        ]
+    ), 
+        React.createElement("div", {id: "child2"}, 
+        [
+            React.createElement("h1", {}, "Im the H1 tag"), 
+            React.createElement("h2", {}, "Im the H2 tag")
+        ]
+    )
+    ]
+)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
