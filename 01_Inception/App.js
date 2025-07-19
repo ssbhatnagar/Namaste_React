19973e82b10cc577d404577116e3be9ebcac5079
S S Bhatnagar <shubhambhatnagar072@gmail.com>
2025-07-20 00:19:29 +0530

    feature/second_episode_full

--- 01_Inception/App.js
+++ 01_Inception/App.js
@@ -1,94 +1,21 @@
-const heading2 = React.createElement( // create element creates an object
-    "h1",
-    {id: "heading"}, // these are attributes
-    "Hello world From React" // this is the children that will go inside the h1 tag 
-);
+import React from 'react';
+import ReactDOM from 'react-dom/client'; // Notice the /client for React 18+
 
-console.log(heading2) // this will give an object
-
-const root2 = ReactDOM.createRoot(document.getElementById("root2"));
-
-root2.render(heading2);  
-
-
-/**
- * Let's create the structure given below it is having parent div which is having child div inside
- * after that we have two siblings that are h1 and h2 that are inside the child
- * to create sibblings use array 
-
-<div id="parent">
-    <div id="child">
-        <h1>Im the h1 tag</h1>
-        <h2>Im the h2 tag</h2>
-    </div>
-</div>
-
-*/
-
-
-
-// creating parent div
-const parent  = React.createElement("div", {id: "parent"},
-    React.createElement("div", {id: "child"}, 
-    // if you want to create siblings then add the children in array like below
-        [
-            React.createElement("h1", {}, "Im the H1 tag"), 
-            React.createElement("h2", {}, "Im the H2 tag")
-        ]
-    )
- )
-const root3 = ReactDOM.createRoot(document.getElementById("root3"));
-root3.render(parent);
-
-// the below code is incorrect because in createElement we have thrid param to create a child
-// const child = React.createElement("div", {id: "child"}, "This is the Children Div")
-// parent.render(child); 
-
-
-/**
- * Now there is another question which arises that what if we have to create the below structure 
- * 
- * <div id="parent">
-    <div id="child">
-        <h1>Im the h1 tag</h1>
-        <h2>Im the h2 tag</h2>
-    </div>
-    <div id="child2">
-        <h1>Im the h1 tag</h1>
-        <h2>Im the h2 tag</h2>
-    </div>
-</div>
-
-If we create the code for above structure it will look like something that is given below which lloks Ugly that is why
-we have something called => JSX
-
-
-const parent  = React.createElement("div", {id: "parent"},
+const parent = React.createElement("div", {id: "parent"},
     [
         React.createElement("div", {id: "child"}, 
-    // if you want to create siblings then add the children in array like below
         [
             React.createElement("h1", {}, "Im the H1 tag"), 
             React.createElement("h2", {}, "Im the H2 tag")
         ]
     ), 
         React.createElement("div", {id: "child2"}, 
-    // if you want to create siblings then add the children in array like below
         [
             React.createElement("h1", {}, "Im the H1 tag"), 
             React.createElement("h2", {}, "Im the H2 tag")
         ]
     )
     ]
- )
-const root3 = ReactDOM.createRoot(document.getElementById("root3"));
-root3.render(parent);
-
-
-
-
- */
-
-
-
- 
+)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
