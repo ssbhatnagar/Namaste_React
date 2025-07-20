f1e67ecac01a4229a2085673d8a0a108643b83b0
S S Bhatnagar <shubhambhatnagar072@gmail.com>
2025-07-21 01:18:42 +0530

    episode 4 food ordering app

--- FooodieZone/App.js
+++ FooodieZone/App.js
@@ -1,38 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client'; // Notice the /client for React 18+
 
-// this is using react create elemement
-// const heading = React.createElement("h1", {id: "heading"}, "Namaste JS Episode 3");
+/**
+ * Here is the basic structure of our food ordering app named as FooodieZone
+ * 
+ * Header
+ *  Logo
+ *  Nav Items 
+ * Body
+ *  Search
+ *  Restaurant Container
+ *      Restaurant card
+ * Footer
+ *  Copyrights
+ *  Links
+ *  Address
+ *  Contact
+ *
+ */
+
+const Header = () =>{
+    return(
+        <div className="header">
+            <div className="logo-container">
+                <img className="logo" src=".\Assets\Logo\FooodieZone Logo.png"/>
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>HOME</li>
+                    <li>About Us</li>
+                    <li>CONTACT US</li>
+                    <li>CART</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+const AppLayout = () =>{
+    return (
+        <div className ="app">
+            <Header/>
+        </div>
+    )
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(heading)
-
-// using jsx the above code can written like below
-// const jsxHeading = <h1 id='heading'> This heading is using JSX</h1>
-// root.render(jsxHeading)
-
-// React functional component 
-const TitleFunctionalComponent = () =>(
-    <h1 className='heading'> Namaste Js from component </h1>
-)
-
-const TitleElement = (
-    <h1 className='heading'> Namaste Js from element </h1>
-)
-
-const str = "This is how you can inject JS code in Compoenent using {}, This whole line is saved as string in JS code"
-
-// This is component composition => composing two components into one another the way TitleFunctionalComponent is used here
-const HeadingComponent = () =>(
-    <div>
-    <TitleFunctionalComponent/> 
-    {str} 
-    {TitleElement}
-    {/* /This is how we can call too THIS IS AMAZING */}
-    {TitleFunctionalComponent()}
-    {"NIMBUZ"} 
-    
-    <h1 className='heading'>This is comming from functional component</h1>
-    </div>
-)
-
-root.render(<HeadingComponent/>) // this is how we render a component
+root.render(<AppLayout/>)
