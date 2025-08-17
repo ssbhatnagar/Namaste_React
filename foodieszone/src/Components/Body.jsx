bce674df1d1237524590ba14ba8594fce6177278
S S Bhatnagar <shubhambhatnagar072@gmail.com>
2025-08-18 00:32:27 +0530

    feat: added filter button

--- foodieszone/src/Components/Body.jsx
+++ foodieszone/src/Components/Body.jsx
@@ -1,21 +1,27 @@
 import RestaurantCard from "./RestaurantCard";
 
 import resList from "../Utils/mockData";
+import { useState } from "react";
 
 
 const Body = () => {
 
-   
+   const [listOfRestaurant, setListOfRestaurant] = useState(resList);
     
 
   return (
     <div className="body">
       <div className="filter"> 
-        <button className="filter-btn"> Top Rated Restaurant </button>
+        <button className="filter-btn" onClick={() => {
+            const filteredList  = listOfRestaurant.filter(
+                (res) => res.avgRating > 4 
+            )
+            setListOfRestaurant(filteredList)
+        }} > Top Rated Restaurant </button>
       </div>
       <div className="res-container">
         {/* ALWAYS GIVE A KEY */}
-        {resList.map((restaurant) => (
+        {listOfRestaurant.map((restaurant) => (
           <RestaurantCard key={restaurant.id} resData={restaurant} />
         ))}
       </div>
