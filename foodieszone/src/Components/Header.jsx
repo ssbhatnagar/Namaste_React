ac056b04878cbf33e5e57e5dee15631f374c2856
S S Bhatnagar <shubhambhatnagar072@gmail.com>
2025-08-26 00:42:26 +0530

    feat: episode-6, searchbar, shimmer

--- foodieszone/src/Components/Header.jsx
+++ foodieszone/src/Components/Header.jsx
@@ -1,6 +1,12 @@
+import { useState } from "react";
 import { LOGO_URL } from "../Utils/constants";
 
 const Header = () => {
+
+  // let btnName = "Login"; // this is just a normal js variable it cannot be use to update the state
+
+  const [btnNameReact, setBtnNameReact] = useState("Login") // this is how a state variable changes the UI
+
     return (
       <div className="header">
         <div className="logo-container">
@@ -12,6 +18,12 @@ const Header = () => {
             <li>About Us</li>
             <li>Contact Us</li>
             <li>Cart</li>
+            <button className="login"
+            onClick={() => { btnNameReact === "Login" ? setBtnNameReact("Logout") : setBtnNameReact("Login")  // once the button is clicked the whole header UI not only the button will re-render
+              console.log(btnNameReact) 
+             }} 
+            
+            >{btnNameReact}</button>
           </ul>
         </div>
       </div>
--- foodieszone/src/Components/RestaurantCard.jsx
+++ foodieszone/src/Components/RestaurantCard.jsx
@@ -1,29 +1,35 @@
 import { CDN_URL } from "../Utils/constants";
 
-const RestaurantCard = (props) => {
-      const { resData } = props;
-    const { cloudinaryImageId, name, cuisines, avgRating, costForTwo } = resData;
-    // eslint-disable-next-line no-unsafe-optional-chaining
-    const { deliveryTime } = resData?.sla; // this is optional chaining, this is just to show case of how its done, but actually this is not right way beacuse it is giving no-unsafe-optional-chaning
-  
-    return (
-      <div className="res-card" style={{ backgroundColor: "#f0f0f0" }}>
-        <img
-          className="res-logo"
-          alt="res-logo"
-          src={
-            CDN_URL +
-            cloudinaryImageId
-          }
-        ></img>
-        <h3>{name}</h3>{" "}
-        <h4>{cuisines.join(", ")}</h4>{" "}
-        {/* join will add , in between the cuisine*/}
-        <h4>{avgRating}</h4>
-        <h4>{costForTwo}</h4>
-        <h4>{deliveryTime} minutes</h4>
-      </div>
-    );
-  };
+const RestaurantCard = ({ resData }) => {
+    // Optional chaining aur default empty object use kiya hai
+    const { 
+        cloudinaryImageId, 
+        name, 
+        cuisines = [], 
+        avgRating, 
+        costForTwo, 
+        sla 
+    } = resData?.info || {};
 
-  export default RestaurantCard;
+    return (
+        <div className="res-card" style={{ backgroundColor: "#f0f0f0" }}>
+            <img
+                className="res-logo"
+                alt="res-logo"
+                src={CDN_URL + cloudinaryImageId}
+            ></img>
+            <h3>{name}</h3>
+            <h4>{cuisines.join(", ")}</h4>
+            <h4>{avgRating} stars</h4>
+            <h4>{costForTwo}</h4>
+            {/* Conditional rendering use kiya hai agar sla ya deliveryTime available na ho */}
+            {sla?.deliveryTime ? (
+                <h4>{sla.deliveryTime} minutes</h4>
+            ) : (
+                <h4>Delivery info not available</h4>
+            )}
+        </div>
+    );
+};
+
+export default RestaurantCard;
